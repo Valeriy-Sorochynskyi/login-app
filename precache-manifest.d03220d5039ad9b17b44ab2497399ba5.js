811217537a7a83a519dd720d51ccd06b5ff13862
Valeriy-Sorochynskyi <haribol162@gmail.com>
2019-04-10 10:16:30 +0300

    Updates

---
+++ precache-manifest.d03220d5039ad9b17b44ab2497399ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "131e1ec9b0f8f0795f32",
+    "url": "/login-app/static/css/main.50ba325d.chunk.css"
+  },
+  {
+    "revision": "131e1ec9b0f8f0795f32",
+    "url": "/login-app/static/js/main.a9f06897.chunk.js"
+  },
+  {
+    "revision": "0d595c1f2f62de080ab0",
+    "url": "/login-app/static/js/runtime~main.8a02c311.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/login-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2f551e50c60c4a0bb91509978ef9b364",
+    "url": "/login-app/index.html"
+  }
+];
